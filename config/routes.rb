7868bc06062f5a7d882f7ede1a178e9538b370a6
Julian Jurai <julian.jurai@ezcater.com>
2018-09-22 19:56:55 -0400

    Portfolio feature (#2)
    * create seeds file for data
    
    * add index action for portfolio
    
    * add actions for controller
    
    * add edit functionality
    
    * add edit and new link buttons
    
    * add portfolio show feature
    
    * implement destory for  portfolio feature
    
    * create custom routes for pages
    
    * customize portfolio routes
    
    * add friendly routes
    
    * update status for enum
    
    * add-toggle-status

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs
+
+  # localhost:3000/about
+  get 'about-me', to: 'pages#about'
+  # localhost:3000/contact
+  get 'contact', to: 'pages#contact'
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
+  # localhost:3000/
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
