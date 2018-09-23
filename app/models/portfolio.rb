062147171aa814c2c43928cb3ca9dd6c71cc8434
Julian <andrewjurai@gmail.com>
2018-09-23 10:45:51 -0400

    implement concerns for manageing placeholder images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   #after Portfolio.new is called
@@ -16,7 +17,7 @@ class Portfolio < ApplicationRecord
 
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height:"600", width: "400")
+    self.thumb_image ||= Placeholder.image_generator(height:"350", width: "200")
   end
 end
---
+++ db/migrate/20180923143512_add_badeg_to_skills.rb
@@ -0,0 +1,5 @@
+class AddBadegToSkills < ActiveRecord::Migration[5.2]
+  def change
+    add_column :skills, :badge, :text
+  end
+end
