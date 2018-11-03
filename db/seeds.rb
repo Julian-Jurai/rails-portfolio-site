47ea41fd9cc7073570e004ccbe22aa19ceca081f
Julian <andrewjurai@gmail.com>
2018-11-03 12:18:49 -0400

    integrated styles for blog show

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ puts "3 Topics  created"
 10.times do |blog|
     Blog.create!(
         title: "My blog ##{blog}",
-        body: "Controls if the diff editor shows changes in leading or trailing whitespace as diffs",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         topic: Topic.all.sample
     )
 end
