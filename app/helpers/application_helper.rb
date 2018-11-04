ee903e7230564ca27dfe9b988be72117936c546f
Julian <andrewjurai@gmail.com>
2018-11-04 10:14:31 -0500

    implemented refactor for nav active states'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
+      (link_to "Register", new_user_registration_path, class: style) + " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
        link_to "Logout", destroy_user_session_path, method: :delete, class: style
@@ -22,5 +22,42 @@ module ApplicationHelper
 
   def copyright_generator
     @copyright = DeveCampViewTool::Renderer.copyright "Julian Jurai", 'All Rights Reserved'
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolios'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
